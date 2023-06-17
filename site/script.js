2c99911297144f4133fded502e9d1208f194f75e
ebuks1993 <111986195+ebuks1993@users.noreply.github.com>
2023-06-17 15:58:39 +0100

    Update script.js

--- site/script.js
+++ site/script.js
@@ -8,10 +8,10 @@ const param2=urlparams.get('amount')
 const param3=urlparams.get('gameid')
 const param4=urlparams.get('name')
 
-console.log(param2,param3,param4)
+// console.log(param2,param3,param4)
 
-document.querySelector('#email-address').value=param1;
-document.querySelector('#amount').value=param2;
+// document.querySelector('#email-address').value=param1;
+// document.querySelector('#amount').value=param2;
 
 
 let dab={"gameid":param3,"name":param4}
@@ -26,8 +26,8 @@ function SquadPay() {
     onSuccess: () => console.log(`Linked successfully`),
     key: "sandbox_pk_8f6b99e03d5e36e735d05fc9c2cf483ad134b0c1cc71",
     //Change key (test_pk_sample-public-key-1) to the key on your Squad Dashboard
-    email:param1,
-    amount:param2*100,
+    email: document.getElementById("email-address").value,
+    amount: document.getElementById("amount").value * 100,
     //Enter amount in Naira or Dollar (Base value Kobo/cent already multiplied by 100)
     currency_code: "NGN",
     metadata:dab
